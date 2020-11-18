803306deba3e3459ecff5222ac0a53153a4a8cb3
lucas.dansay@gmail.com <lucas.dansay@gmail.com>
2020-11-18 11:22:04 +0100

    Non fonctionnel

---
+++ src/main/java/service/EmployeeService.java
@@ -0,0 +1,31 @@
+package service;
+
+import dao.DAO;
+import dao.EmployeeDAO;
+import model.people.Employee;
+
+public class EmployeeService extends Service<Employee> {
+
+    public EmployeeService() {
+        super(new DAO<Employee>(Employee.class));
+    }
+
+    public Employee checkExist(String login) {
+        this.dao.startSession();
+        Employee res = this.getDAO().findByLogin(login);
+        this.dao.closeSession();
+        return res;
+    }
+
+    public Employee checkPassword(String login, String password) {
+        this.dao.startSession();
+        Employee res = this.getDAO().checkPassword(login,password);
+        this.dao.closeSession();
+        return res;
+    }
+
+    @Override
+    protected DAO<Employee> getDAO() {
+        return (EmployeeDAO) this.dao;
+    }
+}
