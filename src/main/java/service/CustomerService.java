286a9f6ff487661d9d938eed6d0f4fbf978be1ee
cboulch <corentinboulch@gmail.com>
2020-11-22 13:50:53 +0100

    [Feat] delete customer

--- src/main/java/service/CustomerService.java
+++ src/main/java/service/CustomerService.java
@@ -2,12 +2,26 @@ package service;
 
 import dao.DAO;
 import model.people.Customer;
-import model.people.Person;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class CustomerService extends Service<Customer> {
 
     public CustomerService() {
         super(new DAO<Customer>(Customer.class));
+    }
+
+    public Map<String, String> deleteCustomer(Customer customer){
+        Map<String, String> errors = new HashMap<>();
+        try{
+            boolean isDeleted = super.delete(customer);
+            if (!isDeleted) errors.put("deletionError", "Impossible de supprimer le client de la base de données");
+        }
+        catch (Exception e){
+            errors.put("dataBaseConnexionError", "Impossible de se connecter à la base de données");
+        }
+        return errors;
     }
 
     @Override
