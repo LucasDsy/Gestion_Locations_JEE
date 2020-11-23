b54c5eec9e3dd1f878d192ae1b187bf54accd680
lucas.dansay@gmail.com <lucas.dansay@gmail.com>
2020-11-23 15:05:15 +0100

    Ajout gestion session #7 - Correctif

--- src/main/java/servlet/AccessFilter.java
+++ src/main/java/servlet/AccessFilter.java
@@ -1,18 +1,18 @@
 package servlet;
 
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletException;
-import javax.servlet.ServletResponse;
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-
 import java.io.IOException;
 
+@WebFilter(
+        urlPatterns = "/views/*",
+        dispatcherTypes = {DispatcherType.REQUEST, DispatcherType.FORWARD},
+        filterName = "AccessFilter"
+)
 public class AccessFilter implements Filter {
 
     private static final String REDIRECT_LOGIN = "/views/employee-login.jsp";
